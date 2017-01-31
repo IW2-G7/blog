4a0f844a66f1b41021f53510e1dd2732896e80c5
Diego <waziri.diego@gmail.com>
2017-01-31 23:47:36 +0100

    Add new project

--- admin/incl/header.php
+++ admin/incl/header.php
@@ -1,64 +1,51 @@
-        <meta content="width=device-width, initial-scale=1" name="viewport"/>
-        <meta charset="UTF-8">
-        <meta name="description" content="Page d'administration" />
-        <meta name="author" content="LMDA" />
-        
-        <!-- Styles -->
-        <link href="assets/plugins/pace-master/themes/blue/pace-theme-flash.css" rel="stylesheet"/>
-        <link href="assets/plugins/uniform/css/default.css" rel="stylesheet"/>
-        <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/plugins/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/plugins/line-icons/simple-line-icons.css" rel="stylesheet" type="text/css"/>	
-        <link href="assets/plugins/offcanvasmenueffects/css/menu_cornerbox.css" rel="stylesheet" type="text/css"/>	
-        <link href="assets/plugins/waves/waves.min.css" rel="stylesheet" type="text/css"/>	
-        <link href="assets/plugins/switchery/switchery.min.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/plugins/3d-bold-navigation/css/style.css" rel="stylesheet" type="text/css"/>	
-        <link href="assets/plugins/slidepushmenus/css/component.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/plugins/datatables/css/jquery.datatables.min.css" rel="stylesheet" type="text/css"/>	
-        <link href="assets/plugins/datatables/css/jquery.datatables_themeroller.css" rel="stylesheet" type="text/css"/>	
-        <link href="assets/plugins/bootstrap-datepicker/css/datepicker3.css" rel="stylesheet" type="text/css"/>
-        
-        <!-- Theme Styles -->
-        <link href="assets/css/meteor.min.css" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/layers/dark-layer.css" class="theme-color" rel="stylesheet" type="text/css"/>
-        <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
-        
-        <script src="assets/plugins/3d-bold-navigation/js/modernizr.js"></script>
-        <script src="assets/plugins/offcanvasmenueffects/js/snap.svg-min.js"></script>
-        
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
-        
-    </head>
-    <body class="compact-menu">
-        <main class="page-content content-wrap">
-            <div class="navbar">
-                <div class="navbar-inner">
-                    <div class="sidebar-pusher">
-                        <a href="javascript:void(0);" class="waves-effect waves-button push-sidebar">
-                            <i class="icon-arrow-right"></i>
-                        </a>
-                    </div>
-                    <div class="logo-box">
-                        <a href="index.html" class="logo-text"><span>Projet GIT</span></a>
-                    </div><!-- Logo Box -->
-                </div>
-            </div><!-- Navbar -->
-            <div class="page-sidebar sidebar">
-                <div class="page-sidebar-inner slimscroll">
-                    <ul class="menu accordion-menu">
-                        <li><a href="index.html" class="waves-effect waves-button"><span class="menu-icon icon-home"></span><p>Accueil</p></a></li>
-                        <li><a href="profile.html" class="waves-effect waves-button"><span class="menu-icon icon-user"></span><p>Profil</p></a></li>
-                        
-                        <li class="droplink active open"><a href="#" class="waves-effect waves-button"><span class="menu-icon icon-grid"></span><p>News</p><span class="arrow"></span><span class="active-page"></span></a>
-                            <ul class="sub-menu">
-                                <li class="active"><a href="liste_news.php">Liste des articles</a></li>
-                            </ul>
-                        </li>
+<nav class="light-green">
+    <div class="container">
+        <div class="nav-wrapper">
+            <a href="index.php" class="brand-logo">Le Meilleur de l'Actualité</a>
+            <?php
+            if($page != 'login' && $page != 'new' && $page != 'password'){
+                ?>
+                    <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
+
+                    <ul class="right hide-on-med-and-down">
+                        <li class="<?php echo ($page=="dashboard")?"active" : ""; ?>"><a href="index.php?page=dashboard"><i class="material-icons">Accueil</i></a></li>
+                        <?php
+                        if(admin()==1){
+                            ?>
+                            <li class="<?php echo ($page=="write")?"active" : ""; ?>"><a href="index.php?page=write"><i class="material-icons">Modifier</i></a></li>
+                            <li class="<?php echo ($page=="list")?"active" : ""; ?>"><a href="index.php?page=list"><i class="material-icons">Voir la liste</i></a></li>
+                            <li class="<?php echo ($page=="settings")?"active" : ""; ?>"><a href="index.php?page=settings"><i class="material-icons">settings</i></a></li>
+
+                        <?php
+                        }
+
+                        ?>
+
+                        <li><a href="../index.php?page=home">Quitter</a></li>
+                        <li><a href="index.php?page=logout">Déconnexion</a></li>
+
                     </ul>
-                </div><!-- Page Sidebar Inner -->
-            </div><!-- Page Sidebar -->
+
+                    <ul class="side-nav" id="mobile-menu">
+                        <li class="<?php echo ($page=="dashboard")?"active" : ""; ?>"><a href="index.php?page=dashboard">Tableau de bord</a></li>
+                        <?php
+
+                        if(admin()==1){
+                            ?>
+                                <li class="<?php echo ($page=="write")?"active" : ""; ?>"><a href="index.php?page=write">Publier un article</a></li>
+                                <li class="<?php echo ($page=="list")?"active" : ""; ?>"><a href="index.php?page=list">Liste des articles</a></li>
+                                <li class="<?php echo ($page=="settings")?"active" : ""; ?>"><a href="index.php?page=settings">Paramètres</a></li>
+                            <?php
+                        }
+
+                        ?>
+                        <li><a href="../index.php?page=home">Quitter</a></li>
+                        <li><a href="index.php?page=logout">Déconnexion</a></li>
+
+                    </ul>
+                <?php
+            }
+            ?>
+        </div>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -1,282 +1,62 @@
 <?php
-  session_start();
+    include 'functions/main-functions.php';
 
-  require "conf.inc.php";
+    $pages = scandir('pages/');
+    if(isset($_GET['page']) && !empty($_GET['page'])){
+        if(in_array($_GET['page'].'.php',$pages)){
+            $page = $_GET['page'];
+        }else{
+            $page = "error";
+        }
+    }else{
+        $page = "home";
+    }
+
+    $pages_functions = scandir('functions/');
+    if(in_array($page.'.func.php',$pages_functions)){
+        include 'functions/'.$page.'.func.php';
+    }
+
 ?>
+
 <!DOCTYPE html>
-<html lang="fr">
+<html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Le Meilleur de l'actualité</title>
+        <title>Le Meilleur de l'Actualité</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta charset=utf-8>
+        <meta name=description content="">
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
         <link href="css/bootstrap.css" rel="stylesheet">
         <link href="css/font-awesome.css" rel="stylesheet">
-        <link href="themetypo.css" rel="stylesheet">
-        <link href="style.css" rel="stylesheet">
+        <link href="css/themetypo.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
         <link href="css/widget.css" rel="stylesheet">
         <link href="color.css" rel="stylesheet">
         <link href="css/flexslider.css" rel="stylesheet">
         <link href="css/owl.carousel.css" rel="stylesheet">
-    	<link href="css/jquery.bxslider.css" rel="stylesheet">    
-    	<link href="css/prettyphoto.css" rel="stylesheet">
+        <link href="css/jquery.bxslider.css" rel="stylesheet">    
+        <link href="css/prettyphoto.css" rel="stylesheet">
         <link href="css/responsive.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
     </head>
+
     <body>
-        <div class="pg-wrapper">
-    	   <?php include('header.php') ?>
-            <div id="mainbanner">
-                <div class="flexslider">
-                    <ul class="slides">
-                        <li>
-                            <img src="http://s.psg.fr/psg/image/phototheque/g/317/63220.jpg" alt="" />
-                            <div class="container">
-                                <div class="pg-caption">       
-                                	<h1>Bienvenue sur Le Meilleur de l'actualité</h1>
-                                    <div class="clearfix"></div>        
-                                  <p>Sed ut perspiciatis unde omnis iste natus <br>error sit accusantium dolore<br>mque laudantium</p>
-                                  <div class="clearfix"></div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://s.psg.fr/psg/image/phototheque/g/317/63268.jpg" alt="" />
-                            <div class="container">
-                                <div class="pg-caption">       
-                                    <h1>La nouvelle version du site est prévue pour janvier 2017</h1> 
-                                    <div class="clearfix"></div>          
-                                    <p>Sed ut perspiciatis unde omnis iste natus <br>error sit accusantium dolore</p>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <img src="http://s.psg.fr/psg/image/phototheque/g/315/62907.jpg" alt="" />
-                            <div class="container">
-                                <div class="pg-caption">       
-                                    <h1>M-4 avant la nouvelle version du site</h1>
-                                    <div class="clearfix"></div>        
-                                    <p>Sed ut perspiciatis unde omnis iste natuserror sit accusantium dolore</p>
-                                    <div class="clearfix"></div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>  
-            <div class="pg-content">
-        		<section class="pick-event padding-30-topbottom margin-top-minus-40">
-                	<div class="container">
-                        <div class="row">
-                        	<div class="col-md-3">
-                                <div class="widget pg-recent-blog">
-                                    <div class="pg-widget-title">
-                                        <h2>Toute l'actualité</h2>
-                                    </div>
-                                    <ul>
-                                        <?php
-                                        for ($i=0; $i < 16; $i++) {
-                                        ?>
-                                          <li>
-                                            <figure>
-                                                <a class="pg-recent-thumb" href="#">
-                                                    <img src="http://s.psg.fr/psg/image/phototheque/g/317/63219.jpg" alt="">
-                                                </a>
-                                              <figcaption>
-                                                <h3> 
-                                                    <a class="news_une" href="#">
-                                                        <span>Actu : </span>Meunier dévoile ses ambitions au PSG 
-                                                    </a>
-                                                </h3>
-                                                <p>Thomas Meunier, arrivé pendant le mercato d'été ne déçoit pas à chaque fois qu'il joue. Il n'a pas connu la moindre défaite avec le PSG.</p>
-                                                <span class="time-news">Hier</span>
-                                            </figure>
-                                          </li>
-                                        <?php
-                                          }
-                                        ?>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="pg-pagecontent col-md-9">
-                                <div class="pg-blog-list pg-grid-blog">
-                                    <ul class="row">
-                                        <div class="col-md-12 pg-section-title"> 
-                                            <h2>Politique</h2>
-                                        </div>
-                                        <?php
-                                            for ($i=0; $i < 3; $i++) { 
-                                        ?>
-                                          <li class="col-md-4">
-                                                <div class="pg-time-zoon"></div>
-                                                <figure>
-                                                    <a href="#">
-                                                        <img src="http://www.lemeilleurdupsg.com/images/miniatures/news/maxune/max_41104.jpg" alt="" title="">
-                                                    </a>
-                                                </figure>
-                                                <div class="pg-blog-info">
-                                                    <time datetime="2008-02-14 20:00">14 <span>avril</span></time>
-                                                    <h3>
-                                                        <a href="#">Sed ut perspiciatis unde omnisiste natus error</a>
-                                                    </h3>
-                                                  
-                                                    <p>Lorem ipsum dolor sit amet, cons ecte tuer adipiscing elit, sed diam non ummy nibh euismod tinc idunt ut laoreet dolore magna ali quam erat volutpat...</p>
-                                                      
-                                                    <div class="clearfix"></div>
+        <?php include 'pages/'.$page.'.php'; ?>
 
-                                                    <ul class="pg-team-network">
-                                                        <li>
-                                                            <a class="fa fa-facebook" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-twitter" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-linkedin" href="#"></a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                          </li>
-                                        <?php
-                                            }
-                                        ?>
-                                  
-                                    <li class="clearfix"></li>
-
-                                    <div class="col-md-12 pg-section-title"> 
-                                        <h2>Sports</h2>
-                                    </div>
-                                        <?php
-                                            for ($i=0; $i < 3; $i++) { 
-                                        ?>
-                                          <li class="col-md-4">
-                                                <div class="pg-time-zoon"></div>
-                                                <figure>
-                                                    <a href="#">
-                                                        <img src="http://www.lemeilleurdupsg.com/images/miniatures/news/maxune/max_41104.jpg" alt="" title="">
-                                                    </a>
-                                                </figure>
-                                                <div class="pg-blog-info">
-                                                    <time datetime="2008-02-14 20:00">14 <span>avril</span></time>
-                                                    <h3>
-                                                        <a href="#">Sed ut perspiciatis unde omnisiste natus error</a>
-                                                    </h3>
-                                                  
-                                                    <p>Lorem ipsum dolor sit amet, cons ecte tuer adipiscing elit, sed diam non ummy nibh euismod tinc idunt ut laoreet dolore magna ali quam erat volutpat...</p>
-                                                      
-                                                    <div class="clearfix"></div>
-
-                                                    <ul class="pg-team-network">
-                                                        <li>
-                                                            <a class="fa fa-facebook" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-twitter" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-linkedin" href="#"></a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                          </li>
-                                        <?php
-                                            }
-                                        ?>
-
-                                    <li class="clearfix"></li>
-
-                                    <div class="col-md-12 pg-section-title"> 
-                                        <h2>Société</h2>
-                                    </div>
-                                        <?php
-                                            for ($i=0; $i < 3; $i++) { 
-                                        ?>
-                                          <li class="col-md-4">
-                                                <div class="pg-time-zoon"></div>
-                                                <figure>
-                                                    <a href="#">
-                                                        <img src="http://www.lemeilleurdupsg.com/images/miniatures/news/maxune/max_41104.jpg" alt="" title="">
-                                                    </a>
-                                                </figure>
-                                                <div class="pg-blog-info">
-                                                    <time datetime="2008-02-14 20:00">14 <span>avril</span></time>
-                                                    <h3>
-                                                        <a href="#">Sed ut perspiciatis unde omnisiste natus error</a>
-                                                    </h3>
-                                                  
-                                                    <p>Lorem ipsum dolor sit amet, cons ecte tuer adipiscing elit, sed diam non ummy nibh euismod tinc idunt ut laoreet dolore magna ali quam erat volutpat...</p>
-                                                      
-                                                    <div class="clearfix"></div>
-
-                                                    <ul class="pg-team-network">
-                                                        <li>
-                                                            <a class="fa fa-facebook" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-twitter" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-linkedin" href="#"></a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                          </li>
-                                        <?php
-                                            }
-                                        ?>
-
-                                    <li class="clearfix"></li>
-
-                                    <div class="col-md-12 pg-section-title"> 
-                                        <h2>Faits divers</h2>
-                                    </div>
-                                        <?php
-                                            for ($i=0; $i < 3; $i++) { 
-                                        ?>
-                                          <li class="col-md-4">
-                                                <div class="pg-time-zoon"></div>
-                                                <figure>
-                                                    <a href="#">
-                                                        <img src="http://www.lemeilleurdupsg.com/images/miniatures/news/maxune/max_41104.jpg" alt="" title="">
-                                                    </a>
-                                                </figure>
-                                                <div class="pg-blog-info">
-                                                    <time datetime="2008-02-14 20:00">14 <span>avril</span></time>
-                                                    <h3>
-                                                        <a href="#">Sed ut perspiciatis unde omnisiste natus error</a>
-                                                    </h3>
-                                                  
-                                                    <p>Lorem ipsum dolor sit amet, cons ecte tuer adipiscing elit, sed diam non ummy nibh euismod tinc idunt ut laoreet dolore magna ali quam erat volutpat...</p>
-                                                      
-                                                    <div class="clearfix"></div>
-
-                                                    <ul class="pg-team-network">
-                                                        <li>
-                                                            <a class="fa fa-facebook" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-twitter" href="#"></a>
-                                                        </li>
-                                                        <li>
-                                                            <a class="fa fa-linkedin" href="#"></a>
-                                                        </li>
-                                                    </ul>
-                                                </div>
-                                          </li>
-                                        <?php
-                                            }
-                                        ?>
-                                </ul>
-                              </div>
-                            </div>
-                    </div>
-                </section>		
-              </div>
-        <?php include('footer.php'); ?>
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="js/materialize.js"></script>
+        <script src="js/script.js"></script>
+        <script src="js/jquery.flexslider.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.countdown.js"></script>  
+        <script src="js/waypoints-min.js"></script>
+        <script src="js/jquery.bxslider.min.js"></script>
+        <script src="js/jquery.accordion.js"></script>
+        <script src="js/jquery.circlechart.js"></script>
+        <script src="js/jquery.prettyphoto.js"></script>
+        <script src="js/kode_pp.js"></script>
+        <script src="js/functions.js"></script>
+    </body>
+</html>
