6b34010b886287e8e75f8fe7d8281374f5ddbded
TharshanSivapalan <sivapalan.tharshan@gmail.com>
2017-02-01 02:05:29 +0100

    correction bug header

--- admin/incl/header.php
+++ admin/incl/header.php
@@ -1,19 +1,18 @@
 <nav class="light-green">
     <div class="container">
         <div class="nav-wrapper">
-            <a href="index.php" class="brand-logo">Le Meilleur de l'Actualité</a>
+            <a href="index.php?page=dashboard" class="brand-logo">Le Meilleur de l'Actualité</a>
             <?php
             if($page != 'login' && $page != 'new' && $page != 'password'){
                 ?>
                     <a href="#" data-activates="mobile-menu" class="button-collapse"><i class="material-icons">menu</i></a>
 
                     <ul class="right hide-on-med-and-down">
-                        <li class="<?php echo ($page=="dashboard")?"active" : ""; ?>"><a href="index.php?page=dashboard"><i class="material-icons">Accueil</i></a></li>
                         <?php
                         if(admin()==1){
                             ?>
-                            <li class="<?php echo ($page=="write")?"active" : ""; ?>"><a href="index.php?page=write"><i class="material-icons">Modifier</i></a></li>
-                            <li class="<?php echo ($page=="list")?"active" : ""; ?>"><a href="index.php?page=list"><i class="material-icons">Voir la liste</i></a></li>
+                            <li class="<?php echo ($page=="write")?"active" : ""; ?>"><a href="index.php?page=write"><i class="material-icons">edit</i></a></li>
+                            <li class="<?php echo ($page=="list")?"active" : ""; ?>"><a href="index.php?page=list"><i class="material-icons">view_list</i></a></li>
                             <li class="<?php echo ($page=="settings")?"active" : ""; ?>"><a href="index.php?page=settings"><i class="material-icons">settings</i></a></li>
 
                         <?php
@@ -29,7 +28,6 @@
                     <ul class="side-nav" id="mobile-menu">
                         <li class="<?php echo ($page=="dashboard")?"active" : ""; ?>"><a href="index.php?page=dashboard">Tableau de bord</a></li>
                         <?php
-
                         if(admin()==1){
                             ?>
                                 <li class="<?php echo ($page=="write")?"active" : ""; ?>"><a href="index.php?page=write">Publier un article</a></li>
