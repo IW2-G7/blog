e5056ccef2e70bcb144b00b811d03ba84804d7ac
Diego <waziri.diego@gmail.com>
2017-02-22 10:33:47 +0100

    Correction slider

--- functions/functions.php
+++ functions/functions.php
@@ -24,11 +24,8 @@ function get_posts(){
                 posts.title,
                 posts.image,
                 posts.date,
-                posts.content,
-                admins.email
+                posts.content
         FROM posts
-        JOIN admins
-        ON posts.writer=admins.email
         WHERE posted='1'
         ORDER BY date DESC
         LIMIT 0,4
